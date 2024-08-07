89c69bb35ff92af47502ccde76a5484d4745055c
Caroline Tenorio <130055209+Carolthe@users.noreply.github.com>
2024-08-07 14:42:30 +0100

    age-calculator

--- age-calculator/src/App.jsx
+++ age-calculator/src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import './App.css'
 import Input from './components/Input'
 import Seta from './Imagens/Seta.png'
@@ -5,22 +6,43 @@ import Paragrafo from './Paragrafo'
 
 function App() {
 
+  const [years, setYears] = useState ("0")
+
+  function ageYears (){
+    const ateste = years
+    const bteste = 2024
+    const result = ateste - bteste
+
+
+   setYears(result)
+  }
+
   return (
 
     <div className='bg-white h-[480px] w-11/12 mx-4 mt-24 p-10 rounded-t-3xl rounded-bl-3xl rounded-br-[110px] '>
    <div className='flex justify-center gap-3'>
     <Input data="DAY"/>
     <Input data="MONTH"/>
-    <Input data="YEAR"/>
+    <Input data="YEAR"
+    value={years} 
+   placeholder
+   onChange={(ev)=> setYears(ev.target.value)}
+    
+    />
     
    </div>
    <div className='grid justify-center mt-8'>
     
     <hr className=''></hr>
-   <img className='w-14 h-14 bg-violet-600 rounded-full ml-24 mb-3' src={Seta} />
-   <Paragrafo age="32" idade="years" />
+    <button
+    onClick={ageYears} className='w-14 h-14 bg-violet-800 rounded-full ml-24 mb-3'>
+   <img className='' src={Seta} />
+   </button>
+   <Paragrafo age={years} idade="years"  />
    <Paragrafo age="3" idade="months" />
-   <Paragrafo age="26" idade="days"/>
+   <Paragrafo age="2" idade="days"
+
+   />
   </div> 
   </div>
   )
