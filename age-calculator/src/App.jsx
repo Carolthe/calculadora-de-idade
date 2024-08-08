e4c88b6511a6e9b259cda3199a8f5ffde4364de7
Caroline Tenorio <130055209+Carolthe@users.noreply.github.com>
2024-08-08 14:34:09 +0100

    age-calculator

--- age-calculator/src/App.jsx
+++ age-calculator/src/App.jsx
@@ -18,8 +18,8 @@ function App() {
 
   return (
 
-    <div className='bg-white h-[480px] w-11/12 mx-4 mt-24 p-10 rounded-t-3xl rounded-bl-3xl rounded-br-[110px] '>
-   <div className='flex justify-center gap-3'>
+    <div className='bg-white h-[480px] w-11/12  sm:my-[150px] sm:w-[650px]  mx-4 mt-24 p-10 rounded-t-3xl rounded-bl-3xl rounded-br-[110px] justify-center grid '>
+   <div className='flex gap-3'>
     <Input data="DAY"/>
     <Input data="MONTH"/>
     <Input data="YEAR"
@@ -35,7 +35,7 @@ function App() {
     <hr className=''></hr>
     <button
     onClick={ageYears} className='w-14 h-14 bg-violet-800 rounded-full ml-24 mb-3'>
-   <img className='' src={Seta} />
+   <img className='' src={Seta} /> 
    </button>
    <Paragrafo age={years} idade="years"  />
    <Paragrafo age="3" idade="months" />
